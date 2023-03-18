f43cb04095d7290d837fd5bfd43f71280e712f17
workavast <workavast@yandex.ru>
2023-03-18 15:32:01 +0300

    Fixes

--- Assets/_Scripts/MovingPlayform/MovingPlatform.cs
+++ Assets/_Scripts/MovingPlayform/MovingPlatform.cs
@@ -12,19 +12,22 @@ public class MovingPlatform : MonoBehaviour
     private CharacterController _characterController;
     private Rigidbody _rigidbody;
     private Vector3 _currentPos;
+    private Vector3 _targetPos;
     
     private void Start()
     {
         _startPos = transform.position;
         _currentPos = _startPos;
         _rigidbody = GetComponent<Rigidbody>();
+        _targetPos = _startPos + new Vector3(_direction * xOffset,0f,0f);
     }
 
     void Update()
     {
-        if (xOffset <= Mathf.Abs(_startPos.x - _currentPos.x))
+        if (_direction * _currentPos.x >= _direction * _targetPos.x)
         {
             _direction *= -1;
+            _targetPos = _startPos + new Vector3(_direction * xOffset, 0, 0);
         }
 
         _currentPos +=  new Vector3(speed * _direction * Time.deltaTime, 0, 0);
